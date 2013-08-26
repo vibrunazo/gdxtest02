33e519300611df3ab8f15e54d840463a5ac9e093
Vandré Israel Brunazo <vibrunazo@gmail.com>
2013-08-26 18:37:31 -0300

    ini

---
+++ gdxtest02-html/src/com/gdxtest02/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.gdxtest02.client;
+
+import com.gdxtest02.GdxTest02;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new GdxTest02();
+	}
+}
