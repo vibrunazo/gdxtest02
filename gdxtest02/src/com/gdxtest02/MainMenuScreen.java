d99e0b987ae712ebef4844ff840319f2c103a1b1
Vandré Israel Brunazo <vibrunazo@gmail.com>
2013-08-27 19:36:49 -0300

    ui start

--- gdxtest02/src/com/gdxtest02/MainMenuScreen.java
+++ gdxtest02/src/com/gdxtest02/MainMenuScreen.java
@@ -2,83 +2,161 @@ package com.gdxtest02;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class MainMenuScreen implements Screen {
 
-        final GdxTest02 game;
+	final GdxTest02 game;
 
-        OrthographicCamera camera;
+	OrthographicCamera camera;
 
-        public MainMenuScreen(final GdxTest02 gam) {
-                game = gam;
+	Skin skin;
+	Stage stage;
 
-                camera = new OrthographicCamera();
-                camera.setToOrtho(false, 800, 480);
+	public MainMenuScreen(final GdxTest02 gam) {
+		game = gam;
 
-        }
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 
+		setupUi();
 
-        @Override
-        public void render(float delta) {
-                Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-                Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-
-                camera.update();
-                game.batch.setProjectionMatrix(camera.combined);
-
-                game.batch.begin();
-                game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
-                game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
-                game.batch.end();
-
-                if (Gdx.input.isTouched()) {
-                        game.setScreen(new GameScreen(game));
-                        dispose();
-                }
-        }
+	}
 
 
-		@Override
-		public void resize(int width, int height) {
-			// TODO Auto-generated method stub
+	private void setupUi() {
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
 
-		}
+		// A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+		// recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+		skin = new Skin();
+
+		// Generate a 1x1 white texture and store it in the skin named "white".
+		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin.add("white", new Texture(pixmap));
+
+		// Store the default libgdx font under the name "default".
+		skin.add("default", new BitmapFont());
+
+		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
+
+		// Create a table that fills the screen. Everything else will go inside this table.
+		Table table = new Table();
+		table.setFillParent(true);
+//		table.setBackground(skin.newDrawable("white", Color.LIGHT_GRAY));
+//		table.setSize(260, 195);
+//		table.setPosition(190, 142);
+		stage.addActor(table);
+
+		// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+		final TextButton button = new TextButton("Click me!", skin);
+		table.add(button);
+
+		// Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
+		// Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
+		// ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
+		// revert the checked state.
+		button.addListener(new ChangeListener() {
+			public void changed (ChangeEvent event, Actor actor) {
+				System.out.println("Clicked! Is checked: " + button.isChecked());
+				button.setText("Good job!");
+				game.setScreen(new GameScreen(game));
+				dispose();
+			}
+		});
+
+		// Add an image actor. Have to set the size, else it would be the size of the drawable (which is the 1x1 texture).
+//		table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
+	}
 
 
-		@Override
-		public void show() {
-			// TODO Auto-generated method stub
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-		}
+		camera.update();
+		game.batch.setProjectionMatrix(camera.combined);
 
+		game.batch.begin();
+		game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+		game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+		game.batch.end();
 
-		@Override
-		public void hide() {
-			// TODO Auto-generated method stub
-
-		}
-
-
-		@Override
-		public void pause() {
-			// TODO Auto-generated method stub
-
-		}
-
-
-		@Override
-		public void resume() {
-			// TODO Auto-generated method stub
-
-		}
+		//		if (Gdx.input.isTouched()) {
+		//			game.setScreen(new GameScreen(game));
+		//			dispose();
+		//		}
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		stage.draw();
+//		Table.drawDebug(stage);
+	}
 
 
-		@Override
-		public void dispose() {
-			// TODO Auto-generated method stub
+	@Override
+	public void resize(int width, int height) {
+		stage.setViewport(width, height, false);
+	}
 
-		}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void dispose() {
+//		game.dispose();
+		stage.dispose();
+		skin.dispose();
+
+	}
 
 }
