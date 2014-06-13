e5d8552ec1b67adf7de2868eaf1f1790c33fd24d
Vandré Brunazo <vibrunazo@gmail.com>
2014-06-12 23:03:41 -0300

    refactored Effects to extend a common class

--- core/src/com/gdxtest02/effects/FireEffect.java
+++ core/src/com/gdxtest02/effects/FireEffect.java
@@ -1,45 +1,13 @@
 package com.gdxtest02.effects;
 
-import com.badlogic.gdx.graphics.g2d.ParticleEffect;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.gdxtest02.AnimRenderer;
-import com.gdxtest02.Effect;
-import com.gdxtest02.util.Util;
-
-public class FireEffect extends Effect {
-
-	private ParticleEffect particle;
-
-	
-	
-	@Override
-	public void update(float delta) {
-		super.update(delta);
-		particle.setPosition(x, y);
-		particle.update(delta);
-	}
-
-	@Override
-	public void draw(SpriteBatch batch) {
-		particle.draw(batch);
-	}
-
-	@Override
-	public void start() {
-		particle = AnimRenderer.getParticleFromName("fire");
-		hasStarted = true;
-		setScale(getScale());
-	}
+public class FireEffect extends SimpleParticleBaseEffect {
 
 	/* (non-Javadoc)
-	 * @see com.gdxtest02.Effect#setScale(float)
+	 * @see com.gdxtest02.effects.SimpleParticleBaseEffect#getParticlename()
 	 */
 	@Override
-	public void setScale(float scale) {
-		super.setScale(scale);
-		if (!hasStarted) return;
-		Util.scaleParticle(particle, scale);
+	public String getParticlename() {
+		return "fire";
 	}
-	
 
 }
--- core/src/com/gdxtest02/effects/HitEffect.java
+++ core/src/com/gdxtest02/effects/HitEffect.java
@@ -1,32 +1,13 @@
 package com.gdxtest02.effects;
 
-import com.badlogic.gdx.graphics.g2d.ParticleEffect;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.gdxtest02.AnimRenderer;
-import com.gdxtest02.Effect;
+public class HitEffect extends SimpleParticleBaseEffect {
 
-public class HitEffect extends Effect {
-
-	private ParticleEffect particle;
-
-	
-	
+	/* (non-Javadoc)
+	 * @see com.gdxtest02.effects.SimpleParticleBaseEffect#getParticlename()
+	 */
 	@Override
-	public void update(float delta) {
-		super.update(delta);
-		particle.setPosition(x, y);
-		particle.update(delta);
+	public String getParticlename() {
+		return "hit";
 	}
-
-	@Override
-	public void draw(SpriteBatch batch) {
-		particle.draw(batch);
-	}
-
-	@Override
-	public void start() {
-		particle = AnimRenderer.getParticleFromName("hit");
-	}
-	
 
 }
