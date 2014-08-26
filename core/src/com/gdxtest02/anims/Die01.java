1c7fdc41e40c29bcb0f3a7462e3806cc7d84207e
Vandré Brunazo <vibrunazo@gmail.com>
2014-08-26 01:05:49 -0300

    added Die01 Anim class

---
+++ core/src/com/gdxtest02/anims/Die01.java
@@ -0,0 +1,34 @@
+package com.gdxtest02.anims;
+
+import com.esotericsoftware.spine.Animation;
+import com.gdxtest02.AnimRenderer;
+import com.gdxtest02.CharAnim;
+
+public class Die01 extends CharAnim {
+
+	private Animation animation;
+
+	public Die01() {
+		super();
+		name = "die01";
+//		setSpeed(2f);
+	}
+
+	@Override
+	protected void ini() {
+		animation = sd.findAnimation(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.gdxtest02.CharAnim#draw()
+	 */
+	@Override
+	public void draw() {
+		super.draw();
+		animation.apply(skeleton, lastTime, animationTime, false, null);
+		if (animationTime > animation.getDuration()) {
+			end();
+		}
+	}
+
+}
